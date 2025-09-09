c11edb3d5fd976cc198c65f54c319c21c42eec90
Rebecca Cristina Vargas do Carmo <rebecca.vargas@software.com.br>
2025-09-09 15:52:17 -0300

    Ajustando venv

--- routes.py
+++ routes.py
@@ -1,14 +1,20 @@
+import unittest
 from main import app
 from flask import render_template
 
-# rotas
-@app.route('/')
-def home():
-    return render_template('home.html')
+class TestSite(unittest.TestCase):
 
-#pagina sobre cachorros
-@app.route('/border')
-def border():
-    return render_template('border.html')
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Bem-vindo ao site sobre cachorros!', response.data)
+
+    def test_border_page(self):
+        response = self.app.get('/border')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Border Collie', response.data) 
 
 
